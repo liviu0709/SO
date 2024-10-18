e06a3024de72b1dd4d04fb40389821331957fa9b
Stoica Liviu <liviu.stoica0709@stud.acs.upb.ro>
2024-10-18 18:50:58 +0300

    Added mmap and remap

--- libc/src/mm/mmap.c
+++ libc/src/mm/mmap.c
@@ -7,13 +7,23 @@
 void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset)
 {
 	/* TODO: Implement mmap(). */
-	return MAP_FAILED;
+    void *ret = (void*)syscall(__NR_mmap, addr, length, prot, flags, fd, offset);
+    if ( ret == MAP_FAILED ) {
+        errno = 1;
+        return MAP_FAILED;
+    }
+	return ret;
 }
 
 void *mremap(void *old_address, size_t old_size, size_t new_size, int flags)
 {
 	/* TODO: Implement mremap(). */
-	return MAP_FAILED;
+    void *ret = (void*)syscall(__NR_mremap, old_address, old_size, new_size, flags);
+    if ( ret == MAP_FAILED ) {
+	    errno = 1;
+        return MAP_FAILED;
+    }
+    return ret;
 }
 
 int munmap(void *addr, size_t length)
