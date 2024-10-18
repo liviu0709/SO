d638f8b8128d2f0f6e214f447fbcb34d1b1fad48
Stoica Liviu <liviu.stoica0709@stud.acs.upb.ro>
2024-10-18 12:19:52 +0300

    Finished string.h

--- libc/src/string/string.c
+++ libc/src/string/string.c
@@ -5,37 +5,66 @@
 char *strcpy(char *destination, const char *source)
 {
 	/* TODO: Implement strcpy(). */
+    char *dst = destination;
+    for ( ; source[0] != '\0' ; source++, dst++ ) {
+        dst[0] = source[0];
+    }
+    dst[0] = '\0';
 	return destination;
 }
 
 char *strncpy(char *destination, const char *source, size_t len)
 {
 	/* TODO: Implement strncpy(). */
+    char *dst = destination;
+    for ( int i = 0 ; i < (int) len ; i++, source++, dst++ ) {
+        dst[0] = source[0];
+    }
 	return destination;
 }
 
 char *strcat(char *destination, const char *source)
 {
 	/* TODO: Implement strcat(). */
+    char *dst = destination;
+    for ( ; dst[0] != '\0' ; dst++ );
+    for ( ; source[0] != '\0' ; dst++, source++) {
+        dst[0] = source[0];
+    }
+    dst[0] = '\0';
 	return destination;
 }
 
 char *strncat(char *destination, const char *source, size_t len)
 {
 	/* TODO: Implement strncat(). */
+    char *dst = destination;
+    for ( ; dst[0] != '\0' ; dst++ );
+    for ( int i = 0 ; i < (int)len ; dst++, source++, i++) {
+        dst[0] = source[0];
+    }
+    dst[0] = '\0';
 	return destination;
 }
 
 int strcmp(const char *str1, const char *str2)
 {
 	/* TODO: Implement strcmp(). */
-	return -1;
+    for ( ; str1[0] != '\0' && str2[0] != '\0' ; str1++, str2++ ) {
+        if ( str1[0] != str2[0] )
+            return str1[0] - str2[0];
+    }
+	return str1[0] - str2[0];
 }
 
 int strncmp(const char *str1, const char *str2, size_t len)
 {
 	/* TODO: Implement strncmp(). */
-	return -1;
+	for ( int i = 0 ; i < (int)len ; str1++, str2++, i++ ) {
+        if ( str1[0] != str2[0] )
+            return str1[0] - str2[0];
+    }
+	return 0;
 }
 
 size_t strlen(const char *str)
@@ -116,23 +145,49 @@ char *strrstr(const char *haystack, const char *needle)
 void *memcpy(void *destination, const void *source, size_t num)
 {
 	/* TODO: Implement memcpy(). */
+    char *dst = (char*)destination;
+    char *src = (char*)source;
+    for ( int i = 0 ; i < (int)num ; i++, dst++, src++ ) {
+        dst[0] = src[0];
+    }
 	return destination;
 }
 
 void *memmove(void *destination, const void *source, size_t num)
 {
 	/* TODO: Implement memmove(). */
+    // to be changed with malloc / free
+    char *src = (char*)source;
+    char buf[num + 10];
+    for ( int i = 0 ; i < (int)num ; i++, src++ ) {
+        buf[i] = src[0];
+    }
+    char *dst = (char*)destination;
+    for ( int i = 0 ; i < (int)num ; i++, dst++ ) {
+        dst[0] = buf[i];
+    }
 	return destination;
 }
 
 int memcmp(const void *ptr1, const void *ptr2, size_t num)
 {
 	/* TODO: Implement memcmp(). */
-	return -1;
+    unsigned char *s1 = (unsigned char*)ptr1;
+    unsigned char *s2 = (unsigned char*)ptr2;
+    for ( int i = 0 ; i < (int)num ; i++, s1++, s2++ ) {
+        if ( s1[0] != s2[0] )
+            return s1[0] - s2[0];
+    }
+	return 0;
 }
 
 void *memset(void *source, int value, size_t num)
 {
 	/* TODO: Implement memset(). */
+    char *src = source;
+    char setter = value;
+    for ( int i = 0 ; i < (int)num ; i++, src++ ) {
+        src[0] = setter;
+    }
 	return source;
 }
