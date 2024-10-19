b88c29d4d1608fc0f5b64b9dedf4c85faf160902
Stoica Liviu <liviu.stoica0709@stud.acs.upb.ro>
2024-10-19 10:48:43 +0300

    Added edge cases on mmap

--- libc/src/mm/mmap.c
+++ libc/src/mm/mmap.c
@@ -1,15 +1,25 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
 #include <sys/mman.h>
+#include <sys/stat.h>
 #include <errno.h>
 #include <internal/syscall.h>
 
 void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset)
 {
 	/* TODO: Implement mmap(). */
+    struct stat idk;
+    if ( fd > 0 && fstat(fd, &idk) < 0 ) {
+        return MAP_FAILED;
+    }
+
+    if ( ((flags & MAP_PRIVATE) == 0) && ((flags & MAP_SHARED) == 0) ) {
+        errno = EINVAL;
+        return MAP_FAILED;
+    }
+
     void *ret = (void*)syscall(__NR_mmap, addr, length, prot, flags, fd, offset);
     if ( ret == MAP_FAILED ) {
-        errno = 1;
         return MAP_FAILED;
     }
 	return ret;
@@ -29,5 +39,10 @@ void *mremap(void *old_address, size_t old_size, size_t new_size, int flags)
 int munmap(void *addr, size_t length)
 {
 	/* TODO: Implement munmap(). */
-	return -1;
+    int ret = syscall(__NR_munmap, addr, length);
+    if ( ret < 0 ) {
+        errno = EINVAL;
+        return -1;
+    }
+	return 0;
 }
