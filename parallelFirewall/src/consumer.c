5670dadb5ae8da8cc4de71d5bd355dd9e2a95c31
Stoica Liviu <liviu.stoica0709@stud.acs.upb.ro>
2024-11-18 10:37:20 +0200

    Just coding style?

--- parallelFirewall/src/consumer.c
+++ parallelFirewall/src/consumer.c
@@ -20,41 +20,28 @@ void consumer_thread(so_consumer_ctx_t *ctx)
 			pthread_cond_wait(ctx->condConsumer, ctx->mutexSync);
 		so_packet_t packet;
 		int ret = ring_buffer_dequeue(ctx->producer_rb, &packet, sizeof(so_packet_t));
-        currentThread++;
+
+		currentThread++;
 		currentThread %= ctx->nrThreads;
 		pthread_mutex_unlock(ctx->mutexSync);
-        pthread_cond_broadcast(ctx->condConsumer);
+		pthread_cond_broadcast(ctx->condConsumer);
 		if (ret == -1) {
 			unsigned long hash = packet_hash(&packet);
 			so_action_t action = process_packet(&packet);
-
 			pthread_mutex_lock(ctx->mutexPrint);
-
-            while ( ctx->threadNum != currentPrint) {
-                pthread_cond_wait(ctx->condPrint, ctx->mutexPrint);
-            }
-
+			while (ctx->threadNum != currentPrint)
+				pthread_cond_wait(ctx->condPrint, ctx->mutexPrint);
 			FILE *out = fopen(ctx->file, "a");
 
 			fprintf(out, "%s %016lx %lu\n", RES_TO_STR(action), hash, packet.hdr.timestamp);
 			fclose(out);
-
-            currentPrint++;
-            currentPrint %= ctx->nrThreads;
-
+			currentPrint++;
+			currentPrint %= ctx->nrThreads;
 			pthread_mutex_unlock(ctx->mutexPrint);
-            pthread_cond_broadcast(ctx->condPrint);
+			pthread_cond_broadcast(ctx->condPrint);
 		}
-
-
-
-
-		// pthread_mutex_lock(ctx->producer_rb->mutexRing);
-		if (ctx->producer_rb->imDone == 1 && ctx->producer_rb->read_pos == ctx->producer_rb->write_pos) {
-			// pthread_mutex_unlock(ctx->producer_rb->mutexRing);
+		if (ctx->producer_rb->imDone == 1 && ctx->producer_rb->read_pos == ctx->producer_rb->write_pos)
 			return;
-		}
-		// pthread_mutex_unlock(ctx->producer_rb->mutexRing);
 	}
 }
 pthread_mutex_t mutex, mutex2, mutexPrint;
@@ -72,9 +59,9 @@ int create_consumers(pthread_t *tids,
 	pthread_mutex_init(&mutex, NULL);
 	pthread_mutex_init(&mutexC, NULL);
 	pthread_mutex_init(&mutex2, NULL);
-    pthread_mutex_init(&mutexPrint, NULL);
+pthread_mutex_init(&mutexPrint, NULL);
 	pthread_cond_init(&condConsumer, NULL);
-    pthread_cond_init(&condPrint, NULL);
+pthread_cond_init(&condPrint, NULL);
 	for (int i = 0; i < num_consumers; i++) {
 		so_consumer_ctx_t *ctx = malloc(sizeof(so_consumer_ctx_t));
 
@@ -86,8 +73,8 @@ int create_consumers(pthread_t *tids,
 		ctx->mutexSync = &mutex;
 		ctx->condConsumer = &condConsumer;
 		ctx->mutexEnd = &mutex2;
-        ctx->condPrint = &condPrint;
-        ctx->mutexPrint = &mutexPrint;
+ctx->condPrint = &condPrint;
+ctx->mutexPrint = &mutexPrint;
 		pthread_create(&tids[i], NULL, (void *)consumer_thread, ctx);
 	}
 	return num_consumers;
